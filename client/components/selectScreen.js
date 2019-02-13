7f33acf4ca5b6a576c72fb3d495777851a204cbb
andrewjohnso7 <andrewjohnso17@gmail.com>
2019-02-12 19:07:07 -0800

    added top and lower halves, need to refactor

--- client/components/selectScreen.js
+++ client/components/selectScreen.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Quick from '../../assets/QuickPlay.png';
+import Bolt from '../../assets/ LightningBolt.png'
 
  const SelectScreen = () => (
    <div className="container">
-     <div className="box-1 box" >Hellloooooo</div>
-     <div className="box-2 box">Hellloooooo</div>
-     <div className="box-3 box">Hellloooooo</div>
-     <div className="box-4 box">Hellloooooo</div>
+   
+     <div className="box-1 box" >
+
+      <div className="upperHalf">
+        <img src={Quick} className="box-img"></img>
+      </div>
+      
+      <div className="lowerHalf">
+        <div>
+          <img src={Bolt} id="bolt"></img>
+        </div>
+        <div>QuickPlay</div>
+      </div>
+      
+     </div>
+     <div className="box-2 box">Arcade</div>
+     <div className="box-3 box">Competitive</div>
+     <div className="box-4 box">Game Browser</div>
    </div>
- )
+ );
+ 
  export default SelectScreen;
